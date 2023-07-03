398dceb6701ab96363d8d1fb08046067d440168c
Sudhesh15 <sudheshholla15@gmail.com>
2023-07-03 14:34:51 +0530

    index session

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ app.post('/login', async (req, res) => {
     isLoggedIn = true;
     jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
       if (err) throw err;
+      req.session.token = token;
       res.cookie('token', token).json({
         id: userDoc._id,
         username,
