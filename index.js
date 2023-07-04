da443a8a10b50ded6f87a344e35fc48598ca60a8
Sudhesh15 <sudheshholla15@gmail.com>
2023-07-05 00:22:07 +0530

    aaa

--- index.js
+++ index.js
@@ -68,24 +68,6 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.get('/profile', (req, res) => {
-  const { token } = req.cookies;
-  console.log("Received token:", token); // Log the received token
-  if (token) {
-    jwt.verify(token, secret, {}, (err, info) => {
-      if (err) {
-        console.error('Token verification error:', err); // Log the error for troubleshooting
-        res.clearCookie('token');
-        res.status(401).json('Invalid token');
-      } else {
-        res.json(info);
-      }
-    });
-  } else {
-    res.status(401).json('Token not found');
-  }
-});
-
 app.post('/logout', (req, res) => {
   isLoggedIn = false;
   res.clearCookie('token');
@@ -93,42 +75,34 @@ app.post('/logout', (req, res) => {
 });
 
 app.post('/post', upload.single('file'), async (req,res) => {
-  const {token} = req.cookies;
-  if(token){
-    jwt.verify(token, secret, {}, async (err,info) => {
-      if (err) throw err;
-      const {productName,productCode,productMrp,productNlc,productHtcBp,brandName,categoryName} = req.body;
-      const postDoc = await Post.create({
-        productName,
-        productCode,
-        productMrp,
-        productNlc,
-        productHtcBp,
-        brandName,
-        categoryName,
-        author:info.id,
-      });
-      res.json(postDoc);
+  if(isLoggedIn){
+    const {productName,productCode,productMrp,productNlc,productHtcBp,brandName,categoryName} = req.body;
+    const postDoc = await Post.create({
+      productName,
+      productCode,
+      productMrp,
+      productNlc,
+      productHtcBp,
+      brandName,
+      categoryName,
+      author:info.id,
     });
+    res.json(postDoc);
   }
 });
 
 app.put('/post', upload.single('file'), async (req,res) => {
-  const {token} = req.cookies;
-  if(token){
-    jwt.verify(token, secret, {}, async (err,info) => {
-      if (err) throw err;
-      await Post.findByIdAndUpdate(req.body.id, {
-        productName : req.body.productName,
-        productCode : req.body.productCode,
-        productMrp : req.body.productMrp,
-        productNlc : req.body.productNlc,
-        productHtcBp : req.body.productHtcBp,
-        brandName : req.body.brandName,
-        categoryName : req.body.categoryName,
-      });
-      displayAlertMessage(res, 'Product updated successfully!');
+  if(isLoggedIn){
+    await Post.findByIdAndUpdate(req.body.id, {
+      productName : req.body.productName,
+      productCode : req.body.productCode,
+      productMrp : req.body.productMrp,
+      productNlc : req.body.productNlc,
+      productHtcBp : req.body.productHtcBp,
+      brandName : req.body.brandName,
+      categoryName : req.body.categoryName,
     });
+    displayAlertMessage(res, 'Product updated successfully!');
   }
 });
 
@@ -142,111 +116,110 @@ function displayAlertMessage(res, message) {
 }
 
 app.delete('/deleteProduct/:id', async (req, res) => {
-  const productId = req.params.id;
-  try {
-    const result = await Post.deleteOne({ _id: new ObjectId(productId) });
-    if (result.deletedCount > 0) {
-      res.status(200).json({ message: 'Product deleted successfully' });
-    } else {
-      res.status(404).json({ error: 'Product not found' });
+  if(isLoggedIn){
+    const productId = req.params.id;
+    try {
+      const result = await Post.deleteOne({ _id: new ObjectId(productId) });
+      if (result.deletedCount > 0) {
+        res.status(200).json({ message: 'Product deleted successfully' });
+      } else {
+        res.status(404).json({ error: 'Product not found' });
+      }
+    } catch (err) {
+      console.error('Error deleting product:', err);
+      res.status(500).json({ error: 'Failed to delete the product' });
     }
-  } catch (err) {
-    console.error('Error deleting product:', err);
-    res.status(500).json({ error: 'Failed to delete the product' });
-  }
+    }
 });
 
 app.get('/getProductOnBrandAndCategory', async (req,res) => {
-  res.json(
-    await Post.find()
-      .populate('productName')
-      .sort({brandName: 1})
-  );
+  if(isLoggedIn){
+    res.json(
+      await Post.find()
+        .populate('productName')
+        .sort({brandName: 1})
+    );
+  }
 });
 
 app.get('/category', async (req,res) => {
-  res.json(
-    await Category.find()
-      .populate('categoryName')
-      .sort({categoryName: 1})
-      .limit(100)
-  );
+  if(isLoggedIn){
+    res.json(
+      await Category.find()
+        .populate('categoryName')
+        .sort({categoryName: 1})
+        .limit(100)
+    );
+  }
 });
 
 app.get('/brand', async (req,res) => {
-  res.json(
-    await Brand.find()
-      .populate('brandName')
-      .sort({brandName: 1})
-      .limit(100)
-  );
+  if(isLoggedIn){
+    res.json(
+      await Brand.find()
+        .populate('brandName')
+        .sort({brandName: 1})
+        .limit(100)
+    );
+  }
 });
 
 app.get('/product/:id', async (req, res) => {
-  const {id} = req.params;
-  const postDoc = await Post.findById(id).populate('categoryName');
-  res.json(postDoc);
+  if(isLoggedIn){
+    const {id} = req.params;
+    const postDoc = await Post.findById(id).populate('categoryName');
+    res.json(postDoc);
+  }
 });
 
 app.get('/category/:category', async (req, res) => {
-  const {category} = req.params;
-  let categoryName = category;
-  const getPostOnCategory = await Post.find({categoryName}).sort({brandName: 1});
-  res.json(getPostOnCategory);
+  if(isLoggedIn){
+    const {category} = req.params;
+    let categoryName = category;
+    const getPostOnCategory = await Post.find({categoryName}).sort({brandName: 1});
+    res.json(getPostOnCategory);
+  }
 });
 
 app.get('/brand/:brand', async (req, res) => {
-  const {brand} = req.params;
-  let brandName = brand;
-  const getPostOnBrand = await Post.find({brandName}).sort({categoryName: 1});
-  res.json(getPostOnBrand);
+  if(isLoggedIn){
+    const {brand} = req.params;
+    let brandName = brand;
+    const getPostOnBrand = await Post.find({brandName}).sort({categoryName: 1});
+    res.json(getPostOnBrand);
+  }
 });
 
 app.post('/createBrand', upload.single('file'), async (req, res) => {
-  const { token } = req.cookies;
-  if(token){
-    jwt.verify(token, secret, {}, async (err, info) => {
-      if (err) {
-        console.error(err);
-        return res.status(401).json({ error: 'Invalid token' });
-      }
-      try {
-        const { brandName } = req.body;
-        const postDoc = await Brand.create({
-          brandName,
-          author: info.id,
-        });
-        res.json(postDoc);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to create brand' });
-      }
-    });
+  if(isLoggedIn){
+    try {
+      const { brandName } = req.body;
+      const postDoc = await Brand.create({
+        brandName,
+        author: info.id,
+      });
+      res.json(postDoc);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to create brand' });
+    }
   }
 });
 
 app.post('/createCategory', upload.single('file'), async (req, res) => {
-  const { token } = req.cookies;
-  if(token){
-    jwt.verify(token, secret, {}, async (err, info) => {
-      if (err) {
-        console.error(err);
-        return res.status(401).json({ error: 'Invalid token' });
-      }
-      try {
-        const { categoryName } = req.body;
-        const postDoc = await Category.create({
-          categoryName,
-          author: info.id,
-        });
-        res.json(postDoc);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Failed to create category' });
-      }
-    });
-  }
+  if(isLoggedIn){
+    try {
+      const { categoryName } = req.body;
+      const postDoc = await Category.create({
+        categoryName,
+        author: info.id,
+      });
+      res.json(postDoc);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Failed to create category' });
+    }
+  } 
 });
-
 
 app.listen(PORT);
