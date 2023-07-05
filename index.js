fb449b8e1bdba90540a1c3ace4bab90028b3c288
Sudhesh15 <sudheshholla15@gmail.com>
2023-07-05 09:10:27 +0530

    htfgh

--- index.js
+++ index.js
@@ -17,9 +17,6 @@ require('dotenv').config()
 const PORT = process.env.PORT || 4000;
 const BASE_URL = process.env.BASE_URL;
 const MONGO_URL = process.env.MONGO_URL;
-const store = require('store2');
-
-console.log("isLoggedIn in localstorage ===>", store.get('isLoggedIn'))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://hollas-price-portal.netlify.app');
